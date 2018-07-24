8446c16cc2a778f70ebd791c5467cb95feccb028
cflambury <carlos.flambury.antelo@gmail.com>
2018-07-24 19:15:24 -0400

    smoothScroll done (scrool of the right way)

--- js/sitio.js
+++ js/sitio.js
@@ -3,3 +3,26 @@
  **************************/
 
 new WOW().init();
+
+/*----------------------------------
+Iniciamos smoothScroll (Scroll Suave)
+--------------------------------*/
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+    offset: 70, // Integer. How far to offset the scrolling anchor location in pixels
+
+});
+
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn();
+        } else {
+            $(".ir-arriba").fadeOut();
+        }
+    });
+
+});
